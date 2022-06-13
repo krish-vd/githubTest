a81124d370f33544cb809ffcf16e9737881e964f
Krish Desai <kvdesai2002@gmail.com>
2022-06-14 02:09:38 +0530

    New code
    Wrote a new code.

---
+++ firstOne.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class firstOne{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a+b);
+    }
+}
